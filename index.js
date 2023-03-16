37cc93d7f44403a94938b9656ef98ac2008be7e3
Romain Crestey <rcrestey@gmail.com>
2023-03-16 12:20:40 +0100

    rename token env var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import { Client, GatewayIntentBits } from 'discord.js';
 
 dotenv.config();
-const token = process.env.TOKEN;
+const token = process.env.DISCORD_BOT_TOKEN;
 const channelId = '1085875010549334036';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
