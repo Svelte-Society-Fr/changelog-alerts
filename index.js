0e46f16df4e2af9a6fa4b8f399f941ce71c9e2f2
Romain Crestey <rcrestey@gmail.com>
2023-03-16 12:17:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+dotenv.config();
+const token = process.env.TOKEN;
+const channelId = '1085875010549334036';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.login(token);
+
+client.on('ready', () => {
+  console.log('Connected');
+
+  const channel = client.channels.cache.get(channelId);
+  channel.send('coucou');
+});
