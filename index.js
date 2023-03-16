8e2deb5cb56c2229b22ed9fd67f9d446be37e79d
Romain Crestey <rcrestey@gmail.com>
2023-03-16 15:18:11 +0100

    log channel id

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import { Client, GatewayIntentBits } from 'discord.js';
 
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
-const channelId = process.env.DISCORD_CHANNEL_ID;
+const channelId = '1085875010549334036';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.login(token);
 
 client.on('ready', async () => {
+  console.log('connected');
   const channel = client.channels.cache.get(channelId);
   await channel.send('coucou');
 
