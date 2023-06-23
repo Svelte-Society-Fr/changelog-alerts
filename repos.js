3575c8b8cb23ce22e638b1001ed78bdf0eb43e52
Romain Crestey <romain.crestey@radiofrance.com>
2023-06-23 08:40:29 +0200

    fix: adjust paths

--- repos.js
+++ repos.js
@@ -6,13 +6,13 @@ export default [
     path: 'packages/kit/CHANGELOG.md',
     color: 'd2d6e1',
     thumbnail:
-      'https://kit.svelte.dev/_app/immutable/assets/svelte-kit-machine.3af040cb.png',
+      'https://kit.svelte.dev/_app/immutable/assets/svelte-kit-machine.3456a340.avif',
   },
   {
     name: 'Svelte',
     owner: 'sveltejs',
     repo: 'svelte',
-    path: 'CHANGELOG.md',
+    path: 'packages/svelte/CHANGELOG.md',
     color: 'ff3e00',
     thumbnail:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png',
